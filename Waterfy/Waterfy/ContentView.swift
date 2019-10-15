aabd1cb1a58443941086454c4e87c3243361c06c
Jully Nobre <jullynobre1@gmail.com>
2019-10-15 10:39:56 -0300

    Add Project

---
+++ Waterfy/Waterfy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Waterfy
+//
+//  Created by Diuli Nobre on 15/10/19.
+//  Copyright © 2019 Diuli Nobre. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Waterfy")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
