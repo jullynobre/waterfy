2ebb3a88e68ebf09edb8c772f79a4ab1cd3051fe
Jully Nobre <jullynobre1@gmail.com>
2019-10-28 15:32:58 -0300

    Add AppIcon

---
+++ Waterfy/Waterfy/Views/New Drink/NewDrink.swift
@@ -0,0 +1,21 @@
+//
+//  NewDrink.swift
+//  Waterfy
+//
+//  Created by Diuli Nobre on 28/10/19.
+//  Copyright © 2019 Diuli Nobre. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewDrink: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewDrink_Previews: PreviewProvider {
+    static var previews: some View {
+        NewDrink()
+    }
+}
