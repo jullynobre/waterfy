5c30576eae00763b58e6066919b74afce744d42f
Jully Nobre <jullynobre1@gmail.com>
2019-10-28 15:33:33 -0300

    Add form to create a new drink

--- Waterfy/Waterfy/Views/New Drink/NewDrink.swift
+++ Waterfy/Waterfy/Views/New Drink/NewDrink.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct NewDrink: View {
+    
+    @State private var enableLogging = false
+    @State private var selectedColor = 0
+    @State private var colors = ["200mL", "250mL", "500mL", "1000mL"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("Drink Size")) {
+                    Picker(selection: $selectedColor, label: Text("Select the Drink Size")) {
+                        ForEach(0 ..< colors.count) {
+                            Text(self.colors[$0]).tag($0)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Swipe down the modal to dismiss")) {
+                    Button(action: {
+                    // activate theme!
+                    }) {
+                        Text("Add Drink")
+                    }
+                }
+            }
+            .navigationBarTitle(Text("New Drink"))
+        }
     }
 }
 
